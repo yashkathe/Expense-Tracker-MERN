e3ea151d5434191f9fccb8424f8b9a3d99c17fa1
Yash Kathe <yashkathe123@gmail.com>
2023-03-11 19:43:29 +0530

    initial commit (original frontend project)

---
+++ Frontend/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import ExpenseForm from "./ExpenseForm";
+
+import styles from './NewExpense.module.css';
+
+const NewExpense = (props) => {
+
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+
+        props.onAddExpense(expenseData);
+    };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
+
+    return (
+        <div className={ styles[ 'new-expense' ] }>
+            { !isEditing && <button onClick={ startEditingHandler } >Add New Expense</button> }
+            { isEditing && <ExpenseForm onSaveExpenseData={ saveExpenseDataHandler } onCancel={ stopEditingHandler } /> }
+        </div>
+    );
+};
+
+export default NewExpense;
---
+++ README.md
@@ -0,0 +1,11 @@
+# Expense Tracker
+
+This project was originally created by [Maximilian Schwarzmüller](https://github.com/mschwarzmueller).
+
+I am adding new functionalities to this project with various additional features and tools such as:  
+- React Router
+- Using custom HTTP Hook to fetch data
+- Creating a new backend from scratch
+- Using mongoDB to store data
+- Implementing Authentication 
+- Framer Motion for animations
